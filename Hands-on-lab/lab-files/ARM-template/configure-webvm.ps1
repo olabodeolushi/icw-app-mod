fc1c63efe00e18f2823cb7ec18049081fa75ec88
Bode Olushi <bode.olushi@inwayz.ca>
2024-03-27 03:16:04 -0400

    Imported source code for Inwayz App Modernization Project

---
+++ Hands-on-lab/lab-files/ARM-template/configure-webvm.ps1
@@ -0,0 +1,103 @@
+param (
+    [Parameter(Mandatory=$False)] [string] $SqlIP = "",
+    [Parameter(Mandatory=$False)] [string] $SqlPass = ""
+)
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+    Stop-Process -Name Explorer -Force
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+
+function Wait-Install {
+    $msiRunning = 1
+    $msiMessage = ""
+    while($msiRunning -ne 0)
+    {
+        try
+        {
+            $Mutex = [System.Threading.Mutex]::OpenExisting("Global\_MSIExecute");
+            $Mutex.Dispose();
+            $DST = Get-Date
+            $msiMessage = "An installer is currently running. Please wait...$DST"
+            Write-Host $msiMessage 
+            $msiRunning = 1
+        }
+        catch
+        {
+            $msiRunning = 0
+        }
+        Start-Sleep -Seconds 1
+    }
+}
+
+# To resolve the error of https://github.com/olabodeolushi/icw-app-mod/issues/68. The cause of the error is Powershell by default uses TLS 1.0 to connect to website, but website security requires TLS 1.2. You can change this behavior with running any of the below command to use all protocols. You can also specify single protocol.
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
+
+# Disable IE ESC
+Disable-InternetExplorerESC
+
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
+$branchName = "main"
+
+# Download and extract the starter solution files
+# ZIP File sometimes gets corrupted
+Write-Host "Downloading icw-app-mod from GitHub" -ForegroundColor Green
+New-Item -ItemType directory -Path C:\MCW
+while((Get-ChildItem -Directory C:\MCW | Measure-Object).Count -eq 0 )
+{
+    (New-Object System.Net.WebClient).DownloadFile("https://github.com/olabodeolushi/icw-app-mod/zipball/$branchName", 'C:\MCW.zip')
+    Expand-Archive -LiteralPath 'C:\MCW.zip' -DestinationPath 'C:\MCW' -Force
+}
+
+
+#rename the random branch name
+$item = get-item "C:\MCW\*"
+Rename-Item $item -NewName "icw-app-mod-$branchName"
+
+# Replace SQL Connection String
+$item = "C:\MCW\icw-app-mod-$branchName"
+Write-Host "Server=$SqlIP;Database=PartsUnlimited;User Id=PUWebSite;Password=$SqlPass;"
+# The config.release.json file is populated with configuration data during compile and release from VS.  config.json is used by the solution on the WebM.
+((Get-Content -path "$item\Hands-on-lab\lab-files\src\src\PartsUnlimitedWebsite\config.release.json" -Raw) -replace 'SETCONNECTIONSTRING',"Server=$SqlIP;Database=PartsUnlimited;User Id=PUWebSite;Password=$SqlPass;") | Set-Content -Path "$item\Hands-on-lab\lab-files\src\src\PartsUnlimitedWebsite\config.json"
+
+# Downloading Deferred Installs
+# Download App Service Migration Assistant 
+(New-Object System.Net.WebClient).DownloadFile('https://appmigration.microsoft.com/api/download/windows/AppServiceMigrationAssistant.msi', 'C:\AppServiceMigrationAssistant.msi')
+# Download Edge 
+# (New-Object System.Net.WebClient).DownloadFile('https://go.microsoft.com/fwlink/?linkid=2108834&Channel=Stable&language=en&brand=M100/MicrosoftEdgeSetup.exe', 'C:\MicrosoftEdgeSetup.exe')
+# Download 3.1.4 SDK
+(New-Object System.Net.WebClient).DownloadFile('https://download.visualstudio.microsoft.com/download/pr/70062b11-491c-403c-91db-9d84462ee292/5db435e39128cbb608e76bf5111ab3dc/dotnet-sdk-3.1.413-win-x64.exe', 'C:\dotnet-sdk-3.1.413-win-x64.exe')
+
+# Schedule Installs for first Logon
+$argument = "-File `"C:\MCW\icw-app-mod-$branchName\Hands-on-lab\lab-files\ARM-template\webvm-logon-install.ps1`""
+$triggerAt = New-ScheduledTaskTrigger -AtLogOn -User demouser
+$action = New-ScheduledTaskAction -Execute "powershell" -Argument $argument 
+Register-ScheduledTask -TaskName "Install Lab Requirements" -Trigger $triggerAt -Action $action -User demouser
+
+# Download Windows Hosting
+Wait-Install
+(New-Object System.Net.WebClient).DownloadFile('https://download.visualstudio.microsoft.com/download/pr/a0da9621-68f0-439a-b617-4697ee0669e3/38eb4aa6e879b9f06b73599ea2e1535f/dotnet-hosting-5.0.10-win.exe', 'C:\dotnet-hosting-5.0.10-win.exe')
+$pathArgs = {C:\dotnet-hosting-5.0.10-win.exe /Install /Quiet /Norestart /Logs logHostingPackage.txt}
+Invoke-Command -ScriptBlock $pathArgs
+
+# Download and install SQL Server Management Studio
+Wait-Install
+(New-Object System.Net.WebClient).DownloadFile('https://aka.ms/ssmsfullsetup', 'C:\SSMS-Setup.exe')
+$pathArgs = {C:\SSMS-Setup.exe /Install /Quiet /Norestart /Logs logSSMS.txt}
+Invoke-Command -ScriptBlock $pathArgs
+
+# Install Git
+Wait-Install
+(New-Object System.Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.30.0.windows.2/Git-2.30.0.2-64-bit.exe', 'C:\Git-2.30.0.2-64-bit.exe')
+Start-Process -file 'C:\Git-2.30.0.2-64-bit.exe' -arg '/VERYSILENT /SUPPRESSMSGBOXES /LOG="C:\git_install.txt" /NORESTART /CLOSEAPPLICATIONS' -passthru | wait-process
+
+# Install VS Code
+Wait-Install
+(New-Object System.Net.WebClient).DownloadFile('https://go.microsoft.com/fwlink/?LinkID=623230', 'C:\vscode.exe')
+Start-Process -file 'C:\vscode.exe' -arg '/VERYSILENT /SUPPRESSMSGBOXES /LOG="C:\vscode_install.txt" /NORESTART /FORCECLOSEAPPLICATIONS /mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"' -passthru | wait-process
+
---
+++ Hands-on-lab/lab-files/ARM-template/webvm-logon-install.ps1
@@ -0,0 +1,52 @@
+function Wait-Install {
+    $msiRunning = 1
+    $msiMessage = ""
+    while($msiRunning -ne 0)
+    {
+        try
+        {
+            $Mutex = [System.Threading.Mutex]::OpenExisting("Global\_MSIExecute");
+            $Mutex.Dispose();
+            $DST = Get-Date
+            $msiMessage = "An installer is currently running. Please wait...$DST"
+            Write-Host $msiMessage 
+            $msiRunning = 1
+        }
+        catch
+        {
+            $msiRunning = 0
+        }
+        Start-Sleep -Seconds 1
+    }
+}
+$branchName = "main"
+# Install App Service Migration Assistant
+Wait-Install
+Write-Host "Installing App Service Migration Assistant..."
+Start-Process -file 'C:\AppServiceMigrationAssistant.msi ' -arg '/qn /l*v C:\asma_install.txt' -passthru | wait-process
+
+# Install Edge
+#Wait-Install
+#Write-Host "Installing Edge..."
+#Start-Process -file 'C:\MicrosoftEdgeSetup.exe' -arg '/qn /l*v C:\edge_install.txt' -passthru | wait-process
+
+# Install .NET Core 3.1 SDK
+Wait-Install
+Write-Host "Installing .NET Core 3.1 SDK..."
+$pathArgs = {C:\dotnet-sdk-3.1.413-win-x64.exe /Install /Quiet /Norestart /Logs logCore31SDK.txt}
+Invoke-Command -ScriptBlock $pathArgs
+
+# Copy Web Site Files
+Wait-Install
+Write-Host "Copying default website files..."
+Expand-Archive -LiteralPath "C:\MCW\icw-app-mod-$branchName\Hands-on-lab\lab-files\web-deploy-files.zip" -DestinationPath 'C:\inetpub\wwwroot' -Force
+
+# Copy the database connection string to the web app.
+Write-Host "Updating config.json with the SQL IP Address and connection string information."
+Copy-Item "C:\MCW\icw-app-mod-$branchName\Hands-on-lab\lab-files\src\src\PartsUnlimitedWebsite\config.json" -Destination 'C:\inetpub\wwwroot' -Force
+
+Unregister-ScheduledTask -TaskName "Install Lab Requirements" -Confirm:$false
+
+# Restart the app for the startup to pick up the database connection string.
+Write-Host "Restarting IIS"
+iisreset.exe /restart
